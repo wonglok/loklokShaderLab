17547c7ab1ffc5cb03371c37aaa23f314dc161a5
Wong Lok <yellowhappy831@gmail.com>
2017-12-08 21:52:40 +0800

    backup on 2017-12-08 21:52:40

--- src/components/parts/SVGConnector/template.js
+++ src/components/parts/SVGConnector/template.js
@@ -1,4 +1,10 @@
-const uuidv4 = require('uuid/v4')
+// const uuidv4 = require('uuid/v4')
+var uuidv4 = function () {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return '_' + Math.random().toString(36).substr(2, 9)
+}
 
 export const getBall = ({ label, name, fromBox, fromBall, defaultValue }) => {
   return {
