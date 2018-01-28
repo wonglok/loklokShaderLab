3c3a8fbc7463c960711fa42da0b5df41ac3a75db
Wong Lok <yellowhappy831@gmail.com>
2018-01-28 08:48:53 +0800

    backup on 2018-01-28 08:48:53

--- src/components/pages/scene-builder/Details/LightPaint.vue
+++ src/components/pages/scene-builder/Details/LightPaint.vue
@@ -13,9 +13,6 @@
   />
 
   <Scene @scene="(v) => { scene = v }">
-    <!-- <SceneReader :doc="doc" ref="sceneReader" /> -->
-    <!-- <LightPaint @paint="(v) => { paint = v }" /> -->
-
   </Scene>
 
 </div>
@@ -48,6 +45,7 @@ export default {
       cam: {
         pos: { x: 3, y: 5, z: 20 }
       },
+      planes: [],
       lights: [],
       ready: false,
       cubeCamera: false,
@@ -78,37 +76,78 @@ export default {
   },
   methods: {
     setup () {
-      var geometry = new THREE.PlaneBufferGeometry(20, 20, 32, 32)
-      var material = new THREE.MeshPhongMaterial({color: 0xffffff, side: THREE.DoubleSide})
-      var plane = new THREE.Mesh(geometry, material)
-      this.scene.add(plane)
+      this.makePlane({
+        pos: new THREE.Vector3(0, 0, 0),
+        opacity: 0.5
+      })
+      this.makePlane({
+        pos: new THREE.Vector3(0, 0, -5.0),
+        opacity: 0.7
+      })
+      this.makePlane({
+        pos: new THREE.Vector3(0, 0, -10.0),
+        opacity: 0.9
+      })
 
-      this.lights.push(this.makeLight({
+      this.makeLight({
         id: 'l1',
         pos: new THREE.Vector3(20, 20, 20),
         color: new THREE.Color(0xff0000)
-      }))
+      })
 
-      this.lights.push(this.makeLight({
+      this.makeLight({
         id: 'l2',
         pos: new THREE.Vector3(-20, 20, 20),
         color: new THREE.Color(0x0000ff)
-      }))
+      })
 
-      this.lights.push(this.makeLight({
+      this.makeLight({
         id: 'l3',
         pos: new THREE.Vector3(10, -20, 20),
         color: new THREE.Color(0x00ff00)
-      }))
+      })
+    },
+    makePlane ({ pos, opacity }) {
+      var geometry = new THREE.PlaneBufferGeometry(20, 20, 1.0, 1.0)
+      var material = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        side: THREE.DoubleSide,
+        transparent: true,
+        opacity
+      })
+      var plane = new THREE.Mesh(geometry, material)
+      plane.position.copy(pos)
+      this.scene.add(plane)
+
+      //
+      this.planes.push(plane)
     },
     makeLight ({ pos, color }) {
       var light = new THREE.PointLight(color, 1, 100)
       light.position.copy(pos)
       this.scene.add(light)
-      return light
+
+      //
+      this.lights.push(light)
+    },
+    run () {
+      var time = Date.now() * 0.0005
+      this.lights[0].position.x = Math.sin(time * 0.7) * 25
+      this.lights[0].position.y = Math.cos(time * 0.5) * 30
+      // this.lights[0].position.z = Math.cos(time * 0.3) * 30
+
+      this.lights[1].position.x = Math.cos(time * 0.3) * 10
+      this.lights[1].position.y = Math.sin(time * 0.5) * 40
+      // this.lights[0].position.z = Math.sin(time * 0.3) * 20
+
+      this.lights[2].position.x = Math.sin(time * 0.9) * 30
+      this.lights[2].position.y = Math.sin(time * 0.5) * 25
+      // this.lights[0].position.z = Math.cos(time * 0.3) * 30
     },
     renderWebGL () {
       TWEEN.update()
+      this.run()
+
       // if (this.paint) {
       //   this.paint.update()
       // }
